527b5b7825128b2e2e0725db6a941cb3ec0ad50b
cutemiya <k-shaipov@mail.ru>
2024-02-19 06:10:17 +0300

    add controller

---
+++ fu/Controllers/BusinessLogicalController.cs
@@ -0,0 +1,21 @@
+﻿using fu.Microservices.BisnesLogicalService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fu.Controllers;
+
+[Route("business")]
+public class BusinessLogicalController: BaseController
+{
+    private readonly IBusinessLogicalService businessLogicalService;
+
+    public BusinessLogicalController(IBusinessLogicalService businessLogicalService)
+    {
+        this.businessLogicalService = businessLogicalService;
+    }
+    
+    [HttpGet("ping123")]
+    public async Task<IActionResult> Ping()
+    {
+        return Ok(await businessLogicalService.Ping());
+    }
+}
