23a6a4c3859c2bab7a551ef59324b87124c0aca6
cutemiya <k-shaipov@mail.ru>
2024-02-19 06:28:28 +0300

    fix service

--- fu/Microservices/BusinessLogicalService/BusinessLogicalService.cs
+++ fu/Microservices/BusinessLogicalService/BusinessLogicalService.cs
@@ -4,12 +4,17 @@ namespace fu.Microservices.BisnesLogicalService;
 
 public class BusinessLogicalService: IBusinessLogicalService
 {
-    private readonly string baseUrl = "bi-service";
+    private readonly string baseUrl = "http://bi-service";
     private readonly HttpClient httpClient = new();
     public async Task<string> Ping()
     {
         var response = await httpClient.GetAsync(new Uri($"{baseUrl}/get123"));
 
-        return response.StatusCode != HttpStatusCode.OK ? "not pong" : "pong";
+        if (response.StatusCode == HttpStatusCode.OK) return "pong";
+        
+        Console.WriteLine(response);
+        Console.WriteLine(response.Content);
+        return "not pong";
+
     }
 }
